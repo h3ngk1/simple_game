c1a2cc47b5af5121bc823a10daa773d050b9c7d3
Hengki <135644783+h3ngk1@users.noreply.github.com>
2023-06-06 01:17:27 +0700

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,19 @@
+class Hero:
+
+    def _init_(self,name,health,attackPower,armorNumber):
+        self.name = name
+        self.health = health
+        self.attackPower = attackPower
+        self.armorNumber = armorNumber
+
+    def serang(self, lawan):
+        print(self.name + 'menyerang' + lawan.name)
+        lawan.diserang(self)
+
+    def diserang(self, lawan):
+        print(self.name + 'diserang', lawan.name)
+    
+sniper = Hero('sniper',100,10,5) 
+rikimaru = Hero('rikimaru',100,5,10)
+
+sniper.serang(rikimaru)
